217f1c119f6c98a9ca28dd41997bc159a9c8b261
Cakir <muhdsyakira@gmail.com>
2026-03-16 16:01:36 +0800

    update to change process name / hotkey

--- force_quit_hitman.py
+++ force_quit_hitman.py
@@ -1,18 +1,21 @@
 import subprocess
 import keyboard
 
+process_name = "HITMAN3.exe" #Change process name here
+hotkey = "alt+f4" #Change hotkey here | alt+f4 | ctrl+alt+f4 | f4 | etc
+
 def quit_program():
     # Force close
-    process_name = "HITMAN3.exe"
     subprocess.run(["taskkill", "/IM", process_name, "/F"], shell=True)
 
 def register_hotkey():
     # Register hotkey
-    keyboard.add_hotkey("alt+f4", quit_program) #Change hotkey here
+    keyboard.add_hotkey(hotkey, quit_program)
 
 def main():
     register_hotkey()
-    print("AltF4_Hitman is currently active")
+    print("Force quit is active")
+    print(f"Press {hotkey} to force quit {process_name}")
     keyboard.wait()
 
 if __name__ == "__main__":
